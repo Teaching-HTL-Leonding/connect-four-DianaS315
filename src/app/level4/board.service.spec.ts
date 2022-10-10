1700566082b7c28b088ee46db94ed4e13a15883f
Diana <dianas@outlook.at>
2022-10-10 15:34:13 +0200

    Added tests for board service

---
+++ src/app/level4/board.service.spec.ts
@@ -0,0 +1,78 @@
+import { BoardService } from "./board.service"
+
+describe('board service', () => {
+  it('can set pieces on the board', () => {
+    const board = new BoardService();
+
+    board.drop(0);
+    board.drop(1);
+
+    expect(board.boardContent[0][5]).toBe(0);
+    expect(board.boardContent[1][5]).toBe(0);
+  })
+
+  it('can find a winner in a row', () => {
+    const board = new BoardService();
+    board.drop(0);
+    board.drop(0);
+    board.drop(1);
+    board.drop(1);
+    board.drop(2);
+    board.drop(2);
+    board.drop(3);
+
+
+    expect(board.getWinnerIndex()).toBe(1);
+  })
+
+  it('can find a winner in a column', () => {
+    const board = new BoardService();
+
+    board.drop(1);
+    board.drop(2);
+    board.drop(1);
+    board.drop(2);
+    board.drop(1);
+    board.drop(2);
+    board.drop(1);
+
+    expect(board.getWinnerIndex()).toBe(1);
+
+  })
+
+  it('can find a winner in an upwards diagonal', () => {
+    const board = new BoardService();
+
+    board.drop(0);
+    board.drop(1);
+    board.drop(1);
+    board.drop(1);
+    board.drop(2);
+    board.drop(2);
+    board.drop(2);
+    board.drop(3);
+    board.drop(3);
+    board.drop(3);
+    board.drop(3);
+
+    expect(board.getWinnerIndex()).toBe(1);
+  })
+
+  it('can find a winner in a downwards diagonal', () => {
+    const board = new BoardService();
+
+    board.drop(6);
+    board.drop(5);
+    board.drop(5);
+    board.drop(5);
+    board.drop(4);
+    board.drop(4);
+    board.drop(4);
+    board.drop(3);
+    board.drop(3);
+    board.drop(3);
+    board.drop(3);
+
+    expect(board.getWinnerIndex()).toBe(1);
+  })
+})
